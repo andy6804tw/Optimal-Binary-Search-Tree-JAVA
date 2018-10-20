c4c3df74d555f21d22798e3b1bd02c6c97731b78
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-10-20 10:25:29 +0800

    add verticle tree

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class BinaryTree {
 
-	Node root;
+	static Node root;
 	int COUNT = 10;
 
 	public void addNode(int key, String name) {
@@ -32,11 +34,7 @@ public class BinaryTree {
 	}
 
 	/**
-	 * 前序走訪
-	 *   2
-	 *  / \
-	 * 1   3 
-	 * 前序 = 2 -> 1 -> 3
+	 * 前序走訪 2 / \ 1 3 前序 = 2 -> 1 -> 3
 	 **/
 	public void preOrderTree(Node focusNode) {
 		if (focusNode != null) {
@@ -47,11 +45,7 @@ public class BinaryTree {
 	}
 
 	/**
-	 * 中序走訪 
-	 *	2
-	 * / \
-	 *1   3 
-	 *中序 = 1 -> 2 -> 3
+	 * 中序走訪 2 / \ 1 3 中序 = 1 -> 2 -> 3
 	 **/
 	public void inOrderTree(Node focusNode) {
 		if (focusNode != null) {
@@ -62,11 +56,7 @@ public class BinaryTree {
 	}
 
 	/**
-	 * 後序走訪 
-	 *  2
-	 * / \ 
-	 *1   3
-	 *後序 = 1 -> 3 -> 2
+	 * 後序走訪 2 / \ 1 3 後序 = 1 -> 3 -> 2
 	 **/
 	public void postOrderTree(Node focusNode) {
 		if (focusNode != null) {
@@ -160,6 +150,11 @@ public class BinaryTree {
 		theTree.postOrderTree(theTree.root); // 後序走訪
 		System.out.println();
 		theTree.print2(theTree.root, 0);
+
+		int height = root.getHeight();
+		System.out.println(height);
+		root.prettyPrint(height);
+
 	}
 }
 
@@ -174,6 +169,85 @@ class Node {
 	Node(int key, String name) {
 		this.key = key;
 		this.name = name;
+	}
+
+	public int getHeight() {
+		return getHeight(this);
+	}
+
+	private int getHeight(Node root) {
+		if (root == null)
+			return 0;
+		return Math.max(getHeight(root.leftChild), getHeight(root.rightChild)) + 1;
+	}
+
+	public void prettyPrint(int height) {
+		System.out.println(prettyPrint(this, 1, height));
+	}
+
+	private StringBuilder prettyPrint(Node root, int currentHeight, int totalHeight) {
+		StringBuilder sb = new StringBuilder();
+		int spaces = getSpaceCount(totalHeight - currentHeight + 1);
+		if (root == null) {
+			// create a 'spatial' block and return it
+			String row = String.format("%" + (2 * spaces + 1) + "s%n", "");
+			// now repeat this row space+1 times
+			String block = new String(new char[spaces + 1]).replace("\0", row);
+			return new StringBuilder(block);
+		}
+		if (currentHeight == totalHeight)
+			return new StringBuilder(root.key + "");
+		int slashes = getSlashCount(totalHeight - currentHeight + 1);
+		sb.append(String.format("%" + (spaces + 1) + "s%" + spaces + "s", root.key + "", ""));
+		sb.append("\n");
+		// now print / and \
+		// but make sure that left and right exists
+		char leftSlash = root.leftChild == null ? ' ' : '/';
+		char rightSlash = root.rightChild == null ? ' ' : '\\';
+		int spaceInBetween = 1;
+		for (int i = 0, space = spaces - 1; i < slashes; i++, space--, spaceInBetween += 2) {
+			for (int j = 0; j < space; j++)
+				sb.append(" ");
+			sb.append(leftSlash);
+			for (int j = 0; j < spaceInBetween; j++)
+				sb.append(" ");
+			sb.append(rightSlash + "");
+			for (int j = 0; j < space; j++)
+				sb.append(" ");
+			sb.append("\n");
+		}
+		// sb.append("\n");
+
+		// now get string representations of left and right subtrees
+		StringBuilder leftTree = prettyPrint(root.leftChild, currentHeight + 1, totalHeight);
+		StringBuilder rightTree = prettyPrint(root.rightChild, currentHeight + 1, totalHeight);
+		// now line by line print the trees side by side
+		Scanner leftScanner = new Scanner(leftTree.toString());
+		Scanner rightScanner = new Scanner(rightTree.toString());
+		// spaceInBetween+=1;
+		while (leftScanner.hasNextLine()) {
+			if (currentHeight == totalHeight - 1) {
+				sb.append(String.format("%-2s %2s", leftScanner.nextLine(), rightScanner.nextLine()));
+				sb.append("\n");
+				spaceInBetween -= 2;
+			} else {
+				sb.append(leftScanner.nextLine());
+				sb.append(" ");
+				sb.append(rightScanner.nextLine() + "\n");
+			}
+		}
+
+		return sb;
+	}
+
+	private int getSlashCount(int height) {
+		if (height <= 3)
+			return height - 1;
+		return (int) (3 * Math.pow(2, height - 3) - 1);
+	}
+
+	private int getSpaceCount(int height) {
+		return (int) (3 * Math.pow(2, height - 2) - 1);
 	}
 
 	public String toString() {
