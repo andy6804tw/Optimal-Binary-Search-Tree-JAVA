4ba9ee59afc14cd60ad0da07edc16d1c8619b3a1
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-10-28 23:32:59 +0800

    modify

--- test.java
+++ test.java
@@ -1,15 +1,6 @@
-import java.util.LinkedHashSet;
-import java.util.Scanner;
-import java.util.Set;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
-
-public class Main {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 
 		Scanner scn = new Scanner(System.in);
-		System.out.println("請輸入每個key被搜尋的機率(以空白隔開):");
 		String value[] = scn.nextLine().split(" "); // 輸入 search key (k[i])的機率
 		int n = value.length, j = 0;
 		double A[][] = new double[n + 2][n + 1], min = Double.MAX_VALUE;
@@ -20,8 +11,8 @@ public class Main {
 			A[i][i] = Double.parseDouble(value[i - 1]); // 自己本身被搜尋的機率
 			R[i][i] = i;
 		}
-		A[n + 1][n] = 0;
-		R[n + 1][n] = 0;
+    A[n+1][n]=0;
+		R[n+1][n]=0;
 
 		for (int diagonal = 1; diagonal <= n - 1; diagonal++) { // 對角線1開始
 			for (int i = 1; i <= n - diagonal; i++) {
@@ -43,7 +34,7 @@ public class Main {
 				System.out.println();
 			}
 		}
-		// 印出陣列A(最佳平均時間)
+    // 印出陣列A(最佳平均時間)
 		for (int i = 1; i < n + 2; i++) {
 			for (int a = 0; a < n + 1; a++) {
 				System.out.printf("%-8.3f", A[i][a]);
@@ -51,35 +42,13 @@ public class Main {
 			System.out.println();
 		}
 		System.out.println();
-		// 印出陣列R(二元樹根節點)
+    // 印出陣列R(二元樹根節點)
 		for (int i = 0; i < n + 2; i++) {
 			for (int a = 0; a < n + 1; a++) {
 				System.out.printf("%d ", R[i][a]);
 			}
 			System.out.println();
 		}
-		// 使用陣列R繪出一棵樹
+    // 使用陣列R繪出一棵樹
 		drawTree(R, value, n);
 	}
-
-	public static void drawTree(int R[][], String value[], int n) {
-		BinaryTree tree = new BinaryTree();
-
-		tree.root = tree.addNode(1, n, R, value);
-		System.out.print("\nPreOrder: ");
-		tree.preOrderTree(tree.root); // 前序走訪的遞迴方法
-		System.out.print("\nInOrder: ");
-		tree.inOrderTree(tree.root); // 中序走訪
-		System.out.print("\nPostOrder: ");
-		tree.postOrderTree(tree.root); // 後序走訪
-		System.out.println();
-		// tree.print2(tree.root, 0);
-
-		int height = tree.root.getHeight();
-		System.out.println("\nPrint Optimal Binary Search Tree:\n");
-		tree.root.prettyPrint(height);
-
-	}
-
-}
-// 0.375 0.375 0.125 0.125
